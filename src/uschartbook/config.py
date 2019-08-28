49e02b7b1ef18f048abf92c1bdb5fcf2a4dbedce
Brian W. Dew <brianwdew@gmail.com>
2019-08-28 12:41:42 -0400

    Updated with new code

---
+++ src/uschartbook/config.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+import os
+import pandas as pd
+import numpy as np
+from uschartbook.api_key import *
+
+data_dir = Path('../chartbook/data/')
+text_dir = Path('../chartbook/text/')
+cps_dir = Path('/home/brian/Documents/CPS/data/clean/')
