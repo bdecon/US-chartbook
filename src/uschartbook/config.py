f0d74d86f44a2134a715effc7b7d18140a39bc15
Brian Dew <brianwdew@gmail.com>
2019-11-25 23:15:01 -0500

    Updated chartbook

--- src/uschartbook/config.py
+++ src/uschartbook/config.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 from uschartbook.api_key import *
 import statsmodels.api as sm
+import re
 import os
 os.environ['X13PATH'] = '/home/brian/Documents/econ_data/micro/x13as/'
 
