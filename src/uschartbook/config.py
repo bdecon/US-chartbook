60a1168559ff96a96691ff9a18c32ae442ee7a12
Brian Dew <brianwdew@gmail.com>
2020-02-02 20:50:13 -0500

    updated chartbook

--- src/uschartbook/config.py
+++ src/uschartbook/config.py
@@ -9,6 +9,15 @@ import itertools
 import os
 os.environ['X13PATH'] = '/home/brian/Documents/econ_data/micro/x13as/'
 
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+from matplotlib.patches import Polygon
+from matplotlib.patches import Rectangle
+from matplotlib.colors import Normalize
+
+plt.rc('font', family='Lato')
+
 from statsmodels.tsa.x13 import x13_arima_analysis
 
 data_dir = Path('../chartbook/data/')
